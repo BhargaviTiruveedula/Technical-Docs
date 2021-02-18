0495ab2f0b05f378b65f2dd194157dfae603e590
BhargaviTiruveedula <79193509+BhargaviTiruveedula@users.noreply.github.com>
2021-02-18 16:50:30 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Github Project'
+copyright = '2021, Bhargavi'
+author = 'Bhargavi'
 
 # The full version, including alpha/beta/rc tags
 release = ''
